0049b4864c3bf35df6e8a23f0558fecce4b85e9d
Your Name <you@example.com>
2017-11-18 16:34:30 +0100

    google test 3 :D

--- test/test.cpp
+++ test/test.cpp
@@ -29,44 +29,20 @@ void printedOnScreen(string s) {
     out = s;
 }
 
-bool TestAnia() {
-    text = "Ania";
-    start();
-    return (out == "Hello " + text + "!");
-}
-
 TEST(Test,TestAnia) {
     text = "Ania";
     start();
     EXPECT_TRUE(out == "Hello " + text + "!");
 }
 
-bool TestEmpty() {
+TEST(Test,TestEmpty) {
     text = "";
     start();
-    return (out == "Hello " + text + "!");
+    EXPECT_TRUE(out == "Hello " + text + "!");
 }
 
-bool TestBasiaItomek() {
+TEST(Test,TestBasiaItomek) {
     text = "Basia i Tomek";
     start();
-    return (out == "Hello " + text + "!");
-}
-
-void test() {
-    std::vector<string> vs;
-    vs.push_back("TestAnia");
-    vs.push_back("TestEmpty");
-    vs.push_back("TestBasiaItomek");
-
-    if (!TestAnia()) {
-        cout << "TestAnia failed!" << endl;
-    } else {
-        cout << "TestAnia OK!" << endl;
-    }
-    if (!TestEmpty()) {
-        cout << "TestEmpty failed!" << endl;
-    } else {
-        cout << "TestEmpty OK!" << endl;
-    }
+    EXPECT_TRUE(out == "Hello " + text + "!");
 }
